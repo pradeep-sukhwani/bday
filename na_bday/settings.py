15e831fc1f4ae85d56f52e9b81ca660769cca735
Pradeep Sukhwani <pradeep.sukhwani@mindtickle.com>
2020-12-19 18:15:56 +0530

    change

--- na_bday/settings.py
+++ na_bday/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'na_bday.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'na_bday.sqlite3'),
     }
 }
 
