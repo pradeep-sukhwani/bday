0d234cfc29416493dc03e0892d8cc1fa85fe95db
Pradeep Sukhwani <pradeep.sukhwani@mindtickle.com>
2020-12-19 21:20:19 +0530

    changes

--- na_bday/settings.py
+++ na_bday/settings.py
@@ -138,7 +138,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "assets/")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
 STATICFILES_DIRS = (os.path.join('static'),)
 
 try:
